a63a32c326c16b5c52cc3f4e43889cad1d0326cc
Jan Buntenbroich <jan@buntenbroich.de>
2026-09-06 20:00:14 +0200

    added header and footer, added ueber-mich and kontakt

---
+++ JavaScript/footer.js
@@ -0,0 +1,17 @@
+// noinspection JSUnusedGlobalSymbols
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer class="footer">
+                <a href="https://kronentroll.de"><img src="../pictures/logo.png" alt="Logo" class="logo-smaller"></a>
+                <nav class="footer-links">
+                    <a href="https://kronentroll.de">Startseite</a>
+                    <a href="https://kronentroll.de/ueber-mich">Über mich</a>
+                    <a href="https://kronentroll.de/kontakt">Kontakt</a> |
+                    &copy 2026 KRONENTROLL
+                </nav>  
+            </footer>
+        `;
+    }
+}
+customElements.define('my-footer', MyFooter);
---
+++ JavaScript/header.js
@@ -0,0 +1,17 @@
+// noinspection JSUnusedGlobalSymbols
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header class="header">
+                <a href="https://kronentroll.de"><img src="../pictures/logo.png" alt="Logo" class="logo-small"></a>
+                <h1>KRONENTROLLs Website</h1>
+                <nav class="header-links">
+                    <a href="https://kronentroll.de">Startseite</a>
+                    <a href="https://kronentroll.de/ueber-mich">Über mich</a>
+                    <a href="https://kronentroll.de/kontakt">Kontakt</a>
+                </nav>
+            </header>
+        `;
+    }
+}
+customElements.define('my-header', MyHeader);
